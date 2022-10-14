3d127d65077910605369f4f6542d921404d48bdb
Afdul Rohmat <afdulrohmat03@gmail.com>
2022-10-15 06:44:34 +0700

    update create textfield + learn about state hoisting anda trailing lambda to get the value inside particular compose component

--- app/src/main/java/com/example/composetipapp/MainActivity.kt
+++ app/src/main/java/com/example/composetipapp/MainActivity.kt
@@ -3,29 +3,40 @@ package com.example.composetipapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.composetipapp.components.InputField
 import com.example.composetipapp.ui.theme.ComposeTipAppTheme
 
 class MainActivity : ComponentActivity() {
+
+    @OptIn(ExperimentalComposeUiApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MyApp {
-                Text(text = "Hello")
+                TopHeader()
+                MainContent()
             }
 
         }
@@ -39,8 +50,7 @@ fun MyApp(content: @Composable () -> Unit) {
     ComposeTipAppTheme {
         // A surface container using the 'background' color from the theme
         Surface(
-            modifier = Modifier.fillMaxSize(),
-            color = MaterialTheme.colors.background
+            modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background
         ) {
             content()
         }
@@ -49,9 +59,9 @@ fun MyApp(content: @Composable () -> Unit) {
 
 }
 
-@Preview()
+//@Preview()
 @Composable
-fun TopHeader() {
+fun TopHeader(totalPerPerson: Double = 0.0) {
     Surface(
         modifier = Modifier
             .fillMaxWidth()
@@ -64,22 +74,71 @@ fun TopHeader() {
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
+            val total = "%.2f".format(totalPerPerson)
             Text(
                 text = "Total Per Person",
-                style = TextStyle(fontWeight = FontWeight.Bold, fontSize = 21.sp)
+                style = TextStyle(fontWeight = FontWeight.Normal, fontSize = 21.sp)
             )
             Spacer(modifier = Modifier.height(8.dp))
             Text(
-                text = "$40.000",
-                style = TextStyle(fontWeight = FontWeight.Bold, fontSize = 40.sp)
+                text = "$$total", style = TextStyle(fontWeight = FontWeight.Bold, fontSize = 40.sp)
             )
 
         }
     }
 }
 
+@ExperimentalComposeUiApi
+//@Preview
+@Composable
+fun MainContent() {
+    BillForm { billAmount ->
+    }
 
-//@Preview(showBackground = true, showSystemUi = true)
+}
+
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable
+fun BillForm(modifier: Modifier = Modifier, onValChange: (String) -> Unit = {}) {
+    val totalBillState = remember {
+        mutableStateOf("")
+    }
+    val validState = remember(totalBillState.value) {
+        totalBillState.value.trim().isNotEmpty()
+    }
+    val keyboardController = LocalSoftwareKeyboardController.current
+
+
+    Surface(
+        modifier = Modifier
+            .padding(2.dp)
+            .fillMaxWidth(),
+        shape = RoundedCornerShape(corner = CornerSize(8.dp)),
+        border = BorderStroke(width = 1.dp, color = Color.LightGray),
+    ) {
+        Column(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            InputField(valueState = totalBillState,
+                labelId = "Enter Bill",
+                enable = true,
+                isSingleLine = true,
+                onAction = KeyboardActions {
+                    if (!validState) return@KeyboardActions
+
+                    //TODO - onvalueChanged
+                    onValChange(totalBillState.value.trim())
+
+                    keyboardController?.hide()
+
+                })
+        }
+    }
+}
+
+
+@Preview(showBackground = true, showSystemUi = true)
+@ExperimentalComposeUiApi
 @Composable
 fun DefaultPreview() {
     ComposeTipAppTheme {
@@ -93,6 +152,8 @@ fun DefaultPreview() {
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
                 TopHeader()
+                Spacer(modifier = Modifier.height(16.dp))
+                MainContent()
             }
         }
 
